bda82f16b40355739874cb9172b3734e811c16b9
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2013-05-13 18:19:48 +0200

    Add examples

---
+++ examples/configure-pythons.pp
@@ -0,0 +1,10 @@
+include python
+include pycharm
+
+python::configure {"Python-2.5":}
+
+python::configure {"Python-2.6":}
+
+python::configure {"Python-2.7":}
+
+python::configure {"Python-3.2":}
---
+++ examples/install-pythons.pp
@@ -0,0 +1,10 @@
+include python
+include pycharm
+
+python::install {"2.5.6": }
+
+python::install {"2.6.8": }
+
+python::install {"2.7.3": }
+
+python::install {"3.2.3": }
